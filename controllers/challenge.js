022291814fb73741b4701b8c2f059b1b42b3558f
ericosg <eric@loadx.org>
2022-07-22 19:24:44 +0300

    corrected calculation logic

--- controllers/challenge.js
+++ controllers/challenge.js
@@ -4,26 +4,33 @@ const DbService = require('../services/db');
 const haversine = require('../helpers/haversine');
 
 class ChallengeController {
-    async process(countPeople) {
+    constructor(people) {
+        this.people = people;
+    }
+
+    async process() {
         const houses = await this.getHouses();
-        const housesWithFees = this.calculateChallenge(houses);
+        const housesWithFees = this.calculate(houses);
         return this.findCheapest(housesWithFees);
     }
 
     async getHouses() {
         return await DbService.execute(async function (listingsAndReviews) {
-            return await listingsAndReviews.find({ property_type: "House", "address.location.coordinates": { $exists: true } }, { projection: { "address.location": 1, price: 1 } }).limit(100).toArray();
+            return await listingsAndReviews.find({ property_type: "House" }, { projection: { "address.location": 1, price: 1, security_deposit: 1, cleaning_fee: 1, extra_people: 1, accommodates: 1 } }).toArray();
         });
     }
 
-    calculateChallenge(houses) {
+    calculate(houses) {
         let pairsList = [];
         for (let i = 0; i < houses.length; i++) {
-            let pairs = { houses: this.pairNearHouses(houses, i) };
-            if (pairs.houses.length) {
-                pairs.houses.push(houses[i]);
-                pairs.fees = this.calculateFees(pairs.houses);
-                pairsList.push(pairs);
+            const house = houses[i];
+            let pairs = { houses: [house] };
+            if (!this.accomodate(pairs, pairsList)) {
+                const pairHouses = this.pairNearHouses(houses, i);
+                if (pairHouses.length) {
+                    pairs.houses.push(...pairHouses);
+                    this.accomodate(pairs, pairsList);
+                }
             }
         }
         return pairsList;
@@ -33,8 +40,29 @@ class ChallengeController {
         return houses.filter((house, i) => i != index && haversine(house.address.location, houses[index].address.location, { threshold: 500, unit: 'meter', format: 'geojson' }));
     }
 
+    accomodate(pairs, pairsList) {
+        if (this.countAccomodation(pairs.houses) >= this.people) {
+            pairs.fees = this.calculateFees(pairs.houses);
+            pairsList.push(pairs);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    countAccomodation(houses) {
+        return houses.reduce((p, c) => p + c.accommodates, 0);
+    }
+
     calculateFees(houses) {
-        return houses.reduce((p, c) => p + Number(c.price ?? 0), 0);
+        return houses.reduce((p, c) => p + this.calculateFee(c), 0);
+    }
+
+    calculateFee(house) {
+        return Number(house.price ?? 0) +
+            Number(house.security_deposit ?? 0) +
+            Number(house.cleaning_fee ?? 0) +
+            Number(house.extra_people ?? 0) * this.people;
     }
 
     findCheapest(list) {
--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,8 @@ const ChallengeController = require('../controllers/challenge')
 router.get('/getCheapestFor/:countPeople', async function (req, res, next) {
   const countPeople = parseInt(req?.params?.countPeople);
   try {
-    let challenge = new ChallengeController();
-    const result = await challenge.process(countPeople);
+    let challenge = new ChallengeController(countPeople);
+    const result = await challenge.process();
     res.json(result);
   } catch (error) {
     res.status(500).json({ error: error.message });
